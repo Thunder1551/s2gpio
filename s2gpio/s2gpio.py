53aa9b20ca66bd71f42fb486e61d77944691d527
Thunder1551 <Hammerschmidt.chris@gmx.de>
2019-10-08 16:31:28 +0200

    Update s2gpio.py

--- s2gpio/s2gpio.py
+++ s2gpio/s2gpio.py
@@ -63,7 +63,8 @@ class S2Gpio(WebSocket):
             #self.pi.set_glitch_filter(pin, 20000)
             #self.pi.set_mode(pin, pigpio.INPUT)
             #self.pi.callback(pin, pigpio.EITHER_EDGE, self.input_callback2)
-            payload = {'report': 'digital_input_change3', 'pin': str(pin), 'level': str(level)}
+            var number = 5
+            payload = {'report': 'digital_input_change3', 'pin': str(pin), 'level': str(number)}
             msg = json.dumps(payload)
             self.sendMessage(msg)
             if state == '0':
