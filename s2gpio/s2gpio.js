dd1a820dbda54778aa7699147958d7f0ad379606
Thunder1551 <Hammerschmidt.chris@gmx.de>
2019-10-09 13:38:48 +0200

    Update s2gpio.js

--- s2gpio/s2gpio.js
+++ s2gpio/s2gpio.js
@@ -69,7 +69,7 @@
             if(reporter === 'digital_input_change3') {
                 var pin = msg['pin'];
                 temp = parseInt(pin);
-		hum = parseInt(msg['level'])
+		hum = 5;
             }
 	    if(reporter === 'write_return') {
         	//var pin = msg['pin'];
