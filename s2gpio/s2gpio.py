568dcde53a891efd7a87ae9d2483bb4dbe816335
Thunder1551 <Hammerschmidt.chris@gmx.de>
2020-02-25 12:28:51 +0100

    Update s2gpio.py

--- s2gpio/s2gpio.py
+++ s2gpio/s2gpio.py
@@ -160,7 +160,8 @@ class S2Gpio(WebSocket):
             pin = int(payload['pin'])
             temp, hum = dht11_pigpio.read(pin)
             ts2 = int(time.time()*1000)
-            payload = {'report': 'temp_data', 'temp': str(temp), 'hum': str(hum), 'ts1': str(ts1), 'ts2': str(ts2)}
+           # payload = {'report': 'temp_data', 'temp': str(temp), 'hum': str(hum), 'ts1': str(ts1), 'ts2': str(ts2)}
+           payload = {'report': 'temp_data', 'temp': str(temp), 'hum': str(hum)}
           #  print('callback', payload)
             msg = json.dumps(payload)
             self.sendMessage(msg)
