a90ac2191fe9ead9ea776bd795a60cb1c06cac83
Thunder1551 <Hammerschmidt.chris@gmx.de>
2020-05-01 12:02:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,10 +9,16 @@ user = os.listdir("/home")
 #pth = '/home/' + user[0]
 pth = '/home/pi/'
 
-call(["wget", "-o", "-P", pth, "https://raw.githubusercontent.com/Thunder1551/s2gpio/master/s2gpio/s2gpio.js"])
-call(["wget", "-o", "-P", "/usr/lib/scratch2/scratch_extensions", "https://raw.githubusercontent.com/Thunder1551/s2gpio/master/s2gpio/s2gpio.js"])
-call(["wget", "-o", "-P", "/usr/lib/scratch2/scratch_extensions", "https://raw.githubusercontent.com/Thunder1551/s2gpio/master/additional_files/extensions.json"])
-call(["wget", "-o", "-P", "/usr/lib/scratch2/medialibrarythumbnails", "https://raw.githubusercontent.com/Thunder1551/s2gpio/master/docs/images/mry.png"])
+call(["curl", "-L", "-o", "/home/pi/s2gpio.js", "https://raw.githubusercontent.com/Thunder1551/s2gpio/master/s2gpio/s2gpio.js"])
+call(["curl", "-L", "-o", "/usr/lib/scratch2/scratch_extensions/s2gpio.js", "https://raw.githubusercontent.com/Thunder1551/s2gpio/master/s2gpio/s2gpio.js"])
+call(["curl", "-L", "-o", "/usr/lib/scratch2/scratch_extensions/extensions.json", "https://raw.githubusercontent.com/Thunder1551/s2gpio/master/additional_files/extensions.json"])
+call(["curl", "-L", "-o", "/usr/lib/scratch2/medialibrarythumbnails/mry.png", "https://raw.githubusercontent.com/Thunder1551/s2gpio/master/docs/images/mry.png"])
+
+
+#call(["wget", "-o", "-P", pth, "https://raw.githubusercontent.com/Thunder1551/s2gpio/master/s2gpio/s2gpio.js"])
+#call(["wget", "-o", "-P", "/usr/lib/scratch2/scratch_extensions", "https://raw.githubusercontent.com/Thunder1551/s2gpio/master/s2gpio/s2gpio.js"])
+#call(["wget", "-o", "-P", "/usr/lib/scratch2/scratch_extensions", "https://raw.githubusercontent.com/Thunder1551/s2gpio/master/additional_files/extensions.json"])
+#call(["wget", "-o", "-P", "/usr/lib/scratch2/medialibrarythumbnails", "https://raw.githubusercontent.com/Thunder1551/s2gpio/master/docs/images/mry.png"])
 
 
 setup(
