d125d75c5ad4eacd9cc5b17001aef5c4054df3e6
Thunder1551 <Hammerschmidt.chris@gmx.de>
2020-05-04 18:53:51 +0200

    Add files via upload

--- s2gpio/test_modules/test_flame.py
+++ s2gpio/test_modules/test_flame.py
@@ -5,8 +5,8 @@ import flame
 
 # To test your sensor uncomment the lines of code that are not relevant.
 # If you are using a PCF8591 on default channel 0x48 and analog Input AIN2. Otherwise change parameters.
-pcf_value = flame.read_pcf8591(0x48,1);
+pcf_value = flame.read_PCF8591(0x48,1);
 print(pcf_value)
 # If you are using a MCP3008 on device 0, port 0 and analog Input AIN0. Otherwise change parameters.
-mcp_value = flame.read_mcp3008(0,0,2);
+mcp_value = flame.read_MCP3008(0,0,0);
 print(mcp_value)
--- s2gpio/test_modules/test_thermistor.py
+++ s2gpio/test_modules/test_thermistor.py
@@ -5,10 +5,10 @@ import thermistor
 
 # To test your sensor uncomment the lines of code that are not relevant.
 # If you are using a PCF8591 on default channel 0x48 and analog Input AIN2. Otherwise change parameters.
-pcf_value = thermistor.read_pcf8591(0x48,2);
+pcf_value = thermistor.read_PCF8591(0x48,2);
 print(pcf_value)
 # If you are using a MCP3008 on device 0, port 0 and analog Input AIN0. Otherwise change parameters.
-mcp_value = thermistor.read_mcp3008(0,0,0);
+mcp_value = thermistor.read_MCP3008(0,0,0);
 print(mcp_value)
   
 
