44d148188bd58c0affa83fcb648ca92cdd581054
Thunder1551 <Hammerschmidt.chris@gmx.de>
2020-05-07 16:28:34 +0200

    Add files via upload

--- s2gpio/test_modules/test_lcd1602_i2c.py
+++ s2gpio/test_modules/test_lcd1602_i2c.py
@@ -36,21 +36,3 @@ except NameError:
     print("LCD_Clear: Not connected")
 except OSError:
     print("not connected or wrong channel")
-"""
-try:
-    #lcd1602_i2c.initialize(0x27)
-    #lcd1602_i2c.write_single_line_message(str(message), 0, 'left_to_right', 1)
-    #lcd1602_i2c.write_single_line_message(str(message), 0, 'normal', 3)
-    #lcd1602_i2c.write_single_line_message(str(message), 0, 'permanent', 3)
-   
-    #lcd1602_i2c.write_double_line_message(str(message), str(message), 'left_to_right', 3)
-    #lcd1602_i2c.write_double_line_message(str(message), str(message), 'normal', 3)
-    #lcd1602_i2c.write_double_line_message(str(message), str(message), 'permanent', 3)
-    
-    #test_single_line_permanent_mode()
-    #test_single_double_left_to_right_mode()
-    
-    #lcd1602_i2c.clear()
-except OSError:
-    print("not connected or wrong channel")
-    """
