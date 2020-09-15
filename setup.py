a001e6270767bef86d1024210545ed69d59d8e63
Thunder1551 <Hammerschmidt.chris@gmx.de>
2020-09-15 17:39:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,13 +17,7 @@ call(["curl", "-L", "-o", "/home/pi/s2gpio.js", "https://raw.githubusercontent.c
 call(["curl", "-L", "-o", "/usr/lib/scratch2/scratch_extensions/s2gpio.js", "https://raw.githubusercontent.com/Thunder1551/s2gpio/master/s2gpio/s2gpio.js"])
 call(["curl", "-L", "-o", "/usr/lib/scratch2/scratch_extensions/extensions.json", "https://raw.githubusercontent.com/Thunder1551/s2gpio/master/additional_files/extensions.json"])
 call(["curl", "-L", "-o", "/usr/lib/scratch2/medialibrarythumbnails/RoboRasp.png", "https://raw.githubusercontent.com/Thunder1551/s2gpio/master/docs/images/RoboRasp.png"])
-
-
-#call(["wget", "-o", "-P", pth, "https://raw.githubusercontent.com/Thunder1551/s2gpio/master/s2gpio/s2gpio.js"])
-#call(["wget", "-o", "-P", "/usr/lib/scratch2/scratch_extensions", "https://raw.githubusercontent.com/Thunder1551/s2gpio/master/s2gpio/s2gpio.js"])
-#call(["wget", "-o", "-P", "/usr/lib/scratch2/scratch_extensions", "https://raw.githubusercontent.com/Thunder1551/s2gpio/master/additional_files/extensions.json"])
-#call(["wget", "-o", "-P", "/usr/lib/scratch2/medialibrarythumbnails", "https://raw.githubusercontent.com/Thunder1551/s2gpio/master/docs/images/mry.png"])
-
+call(["curl", "-L", "-o", "/home/pi/Desktop/s2gpio.desktop", "https://raw.githubusercontent.com/Thunder1551/s2gpio/master/additional_files/s2gpio.desktop"])
 
 setup(
     name='s2gpio',
