a4d148e013a34a5ae13dded5fc392d3757a894d3
Thunder1551 <Hammerschmidt.chris@gmx.de>
2020-09-15 12:36:52 +0200

    Update s2gpio.js

--- s2gpio/s2gpio.js
+++ s2gpio/s2gpio.js
@@ -1,3 +1,30 @@
+/*
+ Copyright (c) 2020 Chris Hammerschmidt All rights reserved.
+ 
+ Note: The basic structure was taken over by Alan Yoricks. The rights to this code remain exclusively with Alan Yoricks.
+ All changes made to the program code are clearly marked with "***RoboRasp --->". If not explicitly marked, the code remains under the following copyright:
+ 
+ Copyright (c) 2016, 2017 Alan Yorinks All rights reserved.
+ 
+ For information about the original project with related documentation please refer to
+ https://github.com/MrYsLab/s2-pi
+ https://mryslab.github.io/s2-pi/
+ 
+ 
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ Version 3 as published by the Free Software Foundation; either
+ or (at your option) any later version.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
+ 
+ You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+ along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 (function (ext) {
     var socket = null;
 
@@ -7,14 +34,8 @@
     var digital_inputs = new Array(32);
     var myStatus = 1; // initially yellow
     var myMsg = 'not_ready';
-    
-    var temp = 2;
-    var hum = 3;
 
-    var bmp_pressure = 0;
-    var bmp_altitude = 0;
-    var dht11_data = 0;
-  
+    // ***RoboRasp -> variables for sensor values
     var flame_data = 0;
     var gas_data = 0;
     var hall_data = 0;
@@ -23,6 +44,9 @@
     var sound_data = 0;
     var thermistor_data = 0;
     var rain_data = 0;
+    var bmp_pressure = 0;
+    var bmp_altitude = 0;
+    var dht11_data = 0;
 
     ext.cnct = function (callback) {
         window.socket = new WebSocket("ws://127.0.0.1:9000");
@@ -55,27 +79,11 @@
             var reporter = msg['report'];
             if(reporter === 'digital_input_change') {
                 var pin = msg['pin'];
-                temp = 4;
-                digital_inputs[parseInt(pin)] = msg['level'];
+                digital_inputs[parseInt(pin)] = msg['level']
             }
-            if(reporter === 'digital_input_change2') {
-                var pin = msg['pin'];
-                temp = 4;
-            }
-            if(reporter === 'digital_input_change3') {
-                var pin = msg['pin'];
-                temp = parseInt(pin);
-                hum = 5;
-            }
-            if(reporter === 'write_return') {
-                //var pin = msg['pin'];
-                //var temporary = msg['level'];
-                //temp = 4;
-                var tempo = msg['pin'];
-                var humtemp = msg['level'];        
-                temp = parseInt(tempo);
-                hum = parseInt(humtemp);
-            }       
+            
+            // ***RoboRasp ---> Begin of handling reporter messages from the server to write received data
+            
             if(reporter === 'temp_data') {
                 var temperature = msg['temp'];
                 temp = parseInt(temperature);
@@ -108,14 +116,16 @@
                 bmp_pressure = msg['bmp_pressure'];
                 bmp_altitude = msg['bmp_altitude'];
             }
-            console.log(message.data);
+            // ***RoboRasp ---> End of handling reporter messages            
+            
+            console.log(message.data)
         };
         window.socket.onclose = function (e) {
             console.log("Connection closed.");
             socket = null;
             connected = false;
             myStatus = 1;
-            myMsg = 'not_ready';
+            myMsg = 'not_ready'
         };
     };
 
@@ -162,38 +172,6 @@
             window.socket.send(msg);
         }
     };
-    
-    // copied digital write block
-    ext.digital_write2 = function (pin, state) {
-        if (connected == false) {
-            alert("Server Not Connected");
-        }
-        console.log("digital write2");
-        // validate the pin number for the mode
-        if (validatePin(pin)) {
-            var msg = JSON.stringify({
-                "command": 'digital_write2', 'pin': pin, 'state': state
-            });
-            console.log(msg);
-            window.socket.send(msg);
-        }
-    };
-    // call websever to return pin number    
-    ext.write = function (pin, state) {
-        if (connected == false) {
-            alert("Server Not Connected");
-        }
-        console.log("write");
-        // validate the pin number for the mode
-        if (validatePin(pin)) {
-            var msg = JSON.stringify({
-                "command": 'write', 'pin': pin, 'state': state
-            });
-            console.log(msg);
-            window.socket.send(msg);
-        }
-    };
-
 
     // when the PWM block is executed
     ext.analog_write = function (pin, value) {
@@ -249,7 +227,7 @@
             }
         }
     };
-
+	
     // when the play tone block is executed
     ext.play_tone = function (pin, frequency) {
         if (connected == false) {
@@ -271,31 +249,24 @@
             alert("Server Not Connected");
         }
         else {
-            return digital_inputs[parseInt(pin)];
+                return digital_inputs[parseInt(pin)]
 
         }
     };
 
-    // when the i2c read block is executed
-    ext.i2c_read = function (model, channel) {
-        if (connected == false) {
-            alert("Server Not Connected");
-        }
-        //validate input model and i2c channel
-        else if (channel === 'Channel' || model === 'MODEL') {
-            alert("Check input of model and channel");
-        }
-        else {
-            console.log("I2C sensor read");
-            var msg = JSON.stringify({
-                "command": "i2c_read", 'model': model, 'channel': channel
-            });
-            console.log(msg);
-            window.socket.send(msg);
-        }
-    };
-  
-    // when the DHT11 sensor value read reporter block is executed
+    /* ***RoboRasp ---> Begin of implemented functions
+    *   dht11_read
+    *   lcd_initialize
+    *   lcd_clear
+    *   lcd_single_line
+    *   lcd_double_line
+    *   i2c_read
+    *   pcf_read
+    *   mcp_read
+    *   joystick_read_pcf8591
+    */
+    
+    // when the dht11_read command block is executed
     ext.dht11read = function (pin, callback) {
         if (connected == false) {
             alert("Server Not Connected");
@@ -308,53 +279,10 @@
             });
             console.log(msg);
             window.socket.send(msg);
-            //window.setTimeout(function() {
-            //callback();
-            //}, 2000);
-        }
-    };
-    // when the DHT11 sensor value read reporter block is executed
-    ext.dht11return = function () {
-        return temp;
-    };
-    // when the Joystick read reporter block is executed
-    ext.joystick = function () {
-        if (connected == false) {
-            alert("Server Not Connected");
-        }
-        console.log("Joystick read");
-        //validate the pin number for the mode
-        var msg = JSON.stringify({
-            "command": "joystick_read"
-        });
-        console.log(msg);
-        window.socket.send(msg);
-        
-    };
-   
-    // when the LCD1602 Block is executed
-    ext.lcd1602 = function (text, line, bool) {
-        if (connected == false) {
-            alert("Server Not Connected");
-        }
-        console.log("write to lcd1602 display");
-        //validate the pin number for the mode
-        if (bool === 'No'){
-            alert("Please check if Display is connected via channel 0x27");
-        }
-        else if (text === 'TEXT'){
-            alert("Please input your Text to display");
-        }
-        else {
-            var msg = JSON.stringify({
-                "command": "lcd1602_write", 'text': text, 'line': line
-            });
-            console.log(msg);
-            window.socket.send(msg);
         }
     };
     
-    // When the LCD Initialize Block is executed
+    // When the lcd_initialize block is executed
     ext.lcd_initialize = function (channel) {
         if (connected == false) {
             alert("Server Not Connected");
@@ -373,7 +301,7 @@
         }
     };
 
-    // When the LCD Clear Block is executed
+    // When the lcd_clear command block is executed
     ext.lcd_clear = function () {
         if (connected == false) {
             alert("Server Not Connected");
@@ -385,8 +313,8 @@
         console.log(msg);
         window.socket.send(msg);
     };
- 
-    // When the LCD Single Line Display Block is executed
+    
+    // When the lcd_single_line command block is executed
     ext.lcd_single_line = function (message, line, mode, duration) {
         if (connected == false) {
             alert("Server Not Connected");
@@ -405,7 +333,7 @@
         }
     };
     
-    // When the LCD Double Line Display Block is executed
+    // When the lcd_double_line command block is executed
     ext.lcd_double_line = function (message0, message1, mode, duration) {
         if (connected == false) {
             alert("Server Not Connected");
@@ -424,32 +352,95 @@
         }
     };
     
-    // when the BMP180 sensor value read block is executed
-    ext.bmp180read = function (bool) {
+    // when the i2c_read command block is executed
+    ext.i2c_read = function (model, channel) {
         if (connected == false) {
             alert("Server Not Connected");
         }
-        console.log("bmp180 read");
-        //validate the pin number for the mode
-        if (bool === 'No') {
-        alert("Please check if BMP sensor is connected via channel 0x77");
+        //validate input model and i2c channel
+        else if (channel === 'Channel' || model === 'MODEL') {
+            alert("Check input of model and channel");
         }
         else {
+            console.log("I2C sensor read");
             var msg = JSON.stringify({
-                "command": "bmp_read", 'bool': bool
+                "command": "i2c_read", 'model': model, 'channel': channel
             });
             console.log(msg);
             window.socket.send(msg);
         }
     };
-
-
-    // general block to return a value of a chosen sensor model
+    
+    // when the pcf_read command block is executed to read an analog sensor value with PCF8591 module
+    ext.pcf_read = function (model, a_pin) {
+        if (connected == false) {
+            alert("Server Not Connected");
+        }
+        else if (model == 'MODEL') {
+            alert("Choose a sensor model");
+        }
+        else if (a_pin == 'PIN') {
+            alert("Choose an input pin");
+        }
+        else {
+            console.log("Analog sensor read: " + model);
+            var msg = JSON.stringify({
+                "command": 'pcf_read', 'model': model, 'a_pin': a_pin
+            });
+            console.log(msg);
+            window.socket.send(msg);
+        }
+    };
+    
+    // when the mcp_read command block is executed to read an analog sensor value with MCP3008 module
+    ext.mcp_read = function (model, a_pin) {
+        if (connected == false) {
+            alert("Server Not Connected");
+        }
+        else if (model == 'MODEL') {
+            alert("Choose a sensor model");
+        }
+        else if (a_pin == 'PIN') {
+            alert("Choose an input pin");
+        }
+        else {
+            console.log("Analog sensor read: " + model);
+            var msg = JSON.stringify({
+                "command": 'mcp_read', 'model': model, 'a_pin': a_pin
+            });
+            console.log(msg);
+            window.socket.send(msg);
+        }
+    };
+    
+    // when the joystick_read_pcf8591 command block is executed to read the joystick direction with PCF8591 module
+    ext.joystick_read_pcf8591 = function (channel, y_pin, x_pin, bt_pin) {
+        if (connected == false) {
+            alert("Server Not Connected");
+        }
+        //validate input pins and i2c channel
+        else if (channel === 'Channel' || y_pin === 'y_pin' || x_pin === 'x_pin' || bt_pin === 'bt_pin') {
+            alert("Check the input pin declaration");
+        }
+        else {
+            console.log("Joystick (PCF8591) read");
+            var msg = JSON.stringify({
+                "command": "joystick_read_pcf8591", 'channel': channel, 'y_pin': y_pin, 'x_pin': x_pin, 'bt_pin': bt_pin
+            });
+            console.log(msg);
+            window.socket.send(msg);
+        }
+    }; 
+    // ***RoboRasp ---> End of implemented functions
+    
+    // ***RoboRasp ---> Begin of read reporter functionality to return a chosen sensor value
+    
+    // when the sensor_return reporter block is executed to return a value of a chosen sensor model
     ext.sensor_return = function (model) {
         if (model === 'MODEL') {
             alert("Choose a sensor model.");
         }
-        else if (model === 'BMP180_Pressure') {
+        else if (model === 'bmp180_Pressure') {
             return bmp_pressure;
         }
         else if (model === 'BMP180_Altitude') {
@@ -483,47 +474,8 @@
             return rain_data;
         }
     };
-
-    // when the Joystick read reporter block is executed
-    ext.pcf_read = function (model, a_pin) {
-        if (connected == false) {
-            alert("Server Not Connected");
-        }
-        else if (model == 'MODEL') {
-            alert("Choose a sensor model");
-        }
-        else if (a_pin == 'PIN') {
-            alert("Choose an input pin");
-        }
-        else {
-            console.log("Analog sensor read: " + model);
-            var msg = JSON.stringify({
-                "command": 'pcf_read', 'model': model, 'a_pin': a_pin
-            });
-            console.log(msg);
-            window.socket.send(msg);
-        }
-    };
-  
-    // when the Joystick read reporter block is executed
-    ext.joystick_read_pcf8591 = function (channel, y_pin, x_pin, bt_pin) {
-        if (connected == false) {
-            alert("Server Not Connected");
-        }
-        //validate input pins and i2c channel
-        else if (channel === 'Channel' || y_pin === 'y_pin' || x_pin === 'x_pin' || bt_pin === 'bt_pin') {
-            alert("Check the input pin declaration");
-        }
-        else {
-            console.log("Joystick (PCF8591) read");
-            var msg = JSON.stringify({
-                "command": "joystick_read_pcf8591", 'channel': channel, 'y_pin': y_pin, 'x_pin': x_pin, 'bt_pin': bt_pin
-            });
-            console.log(msg);
-            window.socket.send(msg);
-        }
-    };
-
+    // ***RoboRasp ---> End of reporter block functionality 
+    
     // general function to validate the pin value
     function validatePin(pin) {
         var rValue = true;
@@ -541,37 +493,28 @@
         return rValue;
     }
 
- var descriptor = {
+    // Block and block menu descriptions
+    var descriptor = {
         blocks: [
             // Block type, block name, function name
-            ["w", 'Connect to s2gpio server.', 'cnct'],
+            ["w", 'Connect to s2_pi server.', 'cnct'],
             [" ", 'Set BCM %n as an Input', 'input','PIN'],
             [" ", "Set BCM %n Output to %m.high_low", "digital_write", "PIN", "0"],
             [" ", "Set BCM PWM Out %n to %n", "analog_write", "PIN", "VAL"],
-            [" ", "Set BCM %n as Servo with angle = %n (0° - 180°)", "servo", "PIN", "0"],     // ***Hackeduca --> Block for Servo
+			[" ", "Set BCM %n as Servo with angle = %n (0° - 180°)", "servo", "PIN", "0"],     // ***Hackeduca --> Block for Servo 			
             [" ", "Tone: BCM %n HZ: %n", "play_tone", "PIN", 1000],
             ["r", "Read Digital Pin %n", "digital_read", "PIN"],
-            [" ", 'Read DHT11 sensor value %n', 'dht11read', 'PIN'],
-            ["r", 'Return DHT11 sensor value', 'dht11return'],
-            [" ", 'PCF8591: Read Joystick', 'joystick'],
-            [" ", "I2C: Read %m.i2c_sensor sensor on channel %m.channel", "i2c_read", "MODEL", "Channel"],
-            ["r", "Return BMP180 sensor value", "bmp180return"],
-            [" ", "Write %n on line %m.high_low LCD1602 Display on 0x27 %m.yes_no", "lcd1602", "TEXT", "0", "No"],
-            [" ", "LCD1602: Initialize Display on %m.channel", "lcd_initialize", "Channel"],
-            [" ", "LCD1602: Clear Display", "lcd_clear"],
-            [" ", "LCD1602 Single-Line Display: %n %m.high_low %m.lcd_mode %n", "lcd_single_line", "Text", "Line", "Mode", "Duration"],
-            [" ", "LCD1602 Double-Line Display: %n %n %m.lcd_mode %n", "lcd_double_line", "Text_0", "Text_1", "Mode", "Duration"],
-            ["r", "Return %m.sensor_model sensor value", "sensor_return", "MODEL"],
-            [" ", "PCF8591: Read %m.analog_sensor at %m.pcf_ai0", "pcf_read", "MODEL", "PIN"],
-            [" ", "PCF8591: Read Joystick %m.channel %m.pcf_ai0 %m.pcf_ai1 %m.pcf_ai2", "joystick_read_pcf8591", "Channel", "y_pin", "x_pin", "bt_pin"]
-
             
-
+            // ***RoboRasp ---> Begin of implemented block descriptions
+            [" ", "PCF8591: Read %m.analog_sensor at %m.pcf_ai0", "pcf_read", "MODEL", "PIN"],
+			["r", "Return %m.sensor_model sensor value", "sensor_return", "MODEL"]
+            // ***RoboRasp ---> End of implemented block descriptions
         ],
         "menus": {
-            "high_low": ["0", "1"],
+            "high_low": ["0", "1"]
+            
+             // ***RoboRasp ---> Begin of implemented menus
             "yes_no": ["No", "Yes"],
-            "adc": ["PCF8591", "MCP3008"],
             "channel": ["0x27", "0x48", "0x77"],
             "pcf_ai0": ["0", "1", "2", "3"],
             "pcf_ai1": ["0", "1", "2", "3"],
@@ -583,11 +526,12 @@
             "lcd_init_clear": ["Initialize", "Clear"],
             "lcd_line": ["single", "double"],
             "lcd_mode": ["normal", "permanent", "right_to_left"]
+             // ***RoboRasp ---> End of implemented block descriptions
 
         },
-        url: 'https://github.com/Thunder1551/s2gpio'
+        url: 'https://github.com/RoboRasp/s2gpio'     // ***RoboRasp ---> url changed
     };
 
     // Register the extension
-    ScratchExtensions.register('s2gpio', descriptor, ext);
+    ScratchExtensions.register('s2gpio', descriptor, ext);       // ***RoboRasp ---> name of extension changed
 })({});
