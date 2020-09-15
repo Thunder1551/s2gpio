e7fb7207fcaa7e24c11060329e9b17d9beb220af
Thunder1551 <Hammerschmidt.chris@gmx.de>
2020-09-15 16:54:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@ from subprocess import call
 
 #call(["pip3", "install", "git+https://github.com/dpallot/simple-websocket-server.git"])
 #call(["pip3", "install", "git+https://github.com/giampaolo/psutil.git"])
+#call(["pip3", "install", "git+https://github.com/adafruit/Adafruit_Python_GPIO.git"])
+#call(["pip3", "install", "git+https://github.com/adafruit/Adafruit_Python_MCP3008.git"])
+#call(["pip3", "install", "git+https://github.com/coding-world/Python_BMP.git"])
+
 
 user = os.listdir("/home")
-#pth = '/home/' + user[0]
-pth = '/home/pi/'
+pth = '/home/' + user[0]
+#pth = '/home/pi/'
 
 call(["curl", "-L", "-o", "/home/pi/s2gpio.js", "https://raw.githubusercontent.com/Thunder1551/s2gpio/master/s2gpio/s2gpio.js"])
 call(["curl", "-L", "-o", "/usr/lib/scratch2/scratch_extensions/s2gpio.js", "https://raw.githubusercontent.com/Thunder1551/s2gpio/master/s2gpio/s2gpio.js"])
